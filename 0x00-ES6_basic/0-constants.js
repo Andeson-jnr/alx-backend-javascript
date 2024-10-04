ae6a67dac455ff34fd17a0e84c9ec71431038032
Andeson-jnr <Andeson-jnr>
2024-10-04 13:01:48 +0000

    0k

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,18 @@
+// Modify taskFirst to use 'const'
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+// No change needed for getLast
+export function getLast() {
+  return ' is okay';
+}
+
+// Modify taskNext to use 'let'
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
